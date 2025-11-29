1ff83f804e12f04f08ff68ed9d95e9757a21ca6a
silaakyz <akyzsila0@gmail.com>
2025-11-29 14:48:05 +0300

    a

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,9 @@
+// src/lib/supabaseClient.ts
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY!;
+// Supabase bilgilerin
+const supabaseUrl = "https://zkjbbqqrjanlfpeybajt.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpramJicXFyamGZwZXliYWp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwOTM1OTUsImV4cCI6MjA3OTY2OTU5NX0.kLhozeMLcLp9kInILAFZ9Qr14zTYSFiyFS8MBl16Y7I";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+// Supabase client oluştur
+export const supabase = createClient(supabaseUrl, supabaseKey);
